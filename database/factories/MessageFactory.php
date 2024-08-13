80e9af732329819bc5b80ab51b2d88058c4a32da
iamdumonde <martinaboumonde@gmail.com>
2024-08-13 09:15:25 +0100

    MessageFactory crée

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -16,22 +16,46 @@ class MessageFactory extends Factory
      */
     public function definition(): array
     {
-        $senderId = fake()->randomElement([0, 1]);
-        if ($senderId === 0) {
-            $senderId = fake()->randomElement(\App\Models\User::where('id', '!=', 1)->pluck('id')->toArray());
-            $receiverId = 1;
-        } else {
-            $receiverId = fake()->randomElement(\App\Models\User::pluck('id')->toArray());
-        }
-        
+        // on sélectionne de manière aléatoire tous les Ids d'utilisateur
+        $userIds = \App\Models\User::pluck('id')->toArray();
+
+        // On décide de manière aléatoire si le message est un message direct (conversation) ou un message de group
+        $isGroupMessage = fake()->boolean(50);
+
+        // on sélectionne un user aléatoirement
+        $senderId = fake()->randomElement($userIds);
+        // On initialise le receiverId et le groupId
+        $receiverId = null;
         $groupId = null;
+
+        // si c'est un message de group
+        if($isGroupMessage) {
+            // On s'assure que le groupe exist dans la BDD
+            $groupIds = \App\Models\Group::pluck('id')->toArray();
+
+            if (empty($groupIds)) {
+                throw new \Exception("Aucun groupe trouvé dans la base de donnée");
+            }
+
+            // on prend au hasard un groupe
+            $groupId = fake()->randomElement($groupIds);
+
+            // Sélectionne un groupe aléatoirement
+            $group = \App\Models\Group::find($groupId);
+            // On récupère un utilisateur du groupe aléatoirement
+            $senderId = fake()->randomElement($group->users->pluck('id')->toArray());
+        } else {
+            // C'est un message direct qu'on envoie
+            // Sélectionne un receiver qui est différent du sender
+            $receiverId = fake()->randomElement(array_diff($userIds, [$senderId])); // array_diff([1, 2, 3, 4, 5], [3]) => [1, 2, 4, 5]);
+        }
 
         return [
             "message" => fake()->realText(),
             "sender_id" => $senderId,
             "receiver_id" => $receiverId,
             "group_id" => $groupId,
-            "conversation_id" => '',
+            // "conversation_id" => '',
         ];
     }
 }
